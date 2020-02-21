801d3b658121cc29f071c1d3a2d109580693b91c
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2020-02-21 17:34:37 +0300

    Updates

---
+++ precache-manifest.d8ebcdbb7e47e0926292771b33afce2c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b417dbf3810017024c9a4cbb608cbb",
+    "url": "/final_project/index.html"
+  },
+  {
+    "revision": "0ae2390d788151a35e24",
+    "url": "/final_project/static/css/main.60fc864f.chunk.css"
+  },
+  {
+    "revision": "8b4afe40a6d18b09d2d6",
+    "url": "/final_project/static/js/2.e22a3389.chunk.js"
+  },
+  {
+    "revision": "0ae2390d788151a35e24",
+    "url": "/final_project/static/js/main.d3f7d402.chunk.js"
+  },
+  {
+    "revision": "31edb970cfe44b8b41a9",
+    "url": "/final_project/static/js/runtime-main.2bbe12c0.js"
+  },
+  {
+    "revision": "c0be6601744e4c2e0fb019be5f58c989",
+    "url": "/final_project/static/media/barber.c0be6601.jpg"
+  },
+  {
+    "revision": "058ef0688a300db68d4aab5afdcb3b20",
+    "url": "/final_project/static/media/fb.058ef068.svg"
+  },
+  {
+    "revision": "6a9b8cf755ce3be62ff68dbefeba732b",
+    "url": "/final_project/static/media/instagram.6a9b8cf7.svg"
+  },
+  {
+    "revision": "b6f2f1103873280f340e4423020b2737",
+    "url": "/final_project/static/media/instruments.b6f2f110.jpg"
+  },
+  {
+    "revision": "28beae04a84ce155228b7e6075e5c8f4",
+    "url": "/final_project/static/media/meditation.28beae04.jpg"
+  },
+  {
+    "revision": "bcd8d26a7f2261000ff62581c1571bfe",
+    "url": "/final_project/static/media/space.bcd8d26a.jpg"
+  },
+  {
+    "revision": "42cf082af6f4abe9be1ddfb837bef9d0",
+    "url": "/final_project/static/media/street.42cf082a.jpg"
+  },
+  {
+    "revision": "feffff85373e526951004800d0b5cc9e",
+    "url": "/final_project/static/media/twitter.feffff85.svg"
+  },
+  {
+    "revision": "dc3fe112aa6a3f1f09d0a7aa35e6ff3c",
+    "url": "/final_project/static/media/union.dc3fe112.jpg"
+  },
+  {
+    "revision": "f8248fbd1e75f92436b31c359c46ce33",
+    "url": "/final_project/static/media/vk.f8248fbd.svg"
+  }
+]);
